771da4bdb5c575017a41714e577f02de50ec81ce
Kumaresan <36756050+kumaresan-subramani@users.noreply.github.com>
2020-02-05 11:06:39 +0530

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,40 +1,22 @@
-import { Button } from '@syncfusion/ej2-buttons';
+import { Button } from "@syncfusion/ej2-buttons";
 
 // Initialize Button component.
-let button: Button = new Button({ content: 'Button' });
+let button: Button = new Button({ content: "Button" });
 
 // Render initialized Button.
-button.appendTo('#element');
+button.appendTo("#element");
 
-let data: any = [ {
-  OrderID: 10248, CustomerID: 'VINET', EmployeeID: 5, OrderDate: new Date(8364186e5),
-  ShipName: 'Vins et alcools Chevalier', ShipCity: 'Reims', ShipAddress: '59 rue de l Abbaye',
-  ShipRegion: 'CJ', ShipPostalCode: '51100', ShipCountry: 'France', Freight: 32.38, Verified: !0
-},
-{
-  OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6, OrderDate: new Date(836505e6),
-  ShipName: 'Toms Spezialitäten', ShipCity: 'Münster', ShipAddress: 'Luisenstr. 48',
-  ShipRegion: 'CJ', ShipPostalCode: '44087', ShipCountry: 'Germany', Freight: 11.61, Verified: !1
-},]
-
-document.getElementById('element').addEventListener("click", function(){
+document.getElementById("element").addEventListener("click", function() {
   check();
 });
 
-function check() {
+async function check() {
+  // const Grid = await import('@syncfusion/ej2-grids');
 
-import("@syncfusion/ej2-grids").then((Grid) => {
+  // import(/* webpackIgnore: true */ '@syncfusion/ej2-grids');
 
-  let grid: Grid = new Grid({
-    dataSource: data,
-    columns: [
-                { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, type: 'number' },
-                { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
-                { field: 'Freight', headerText: 'Freight', textAlign: 'Right', width: 120, format: 'C' },
-                { field: 'OrderDate', headerText: 'Order Date', width: 140, format: 'yMd' }
-    ],
-    height: 315
+  import("@syncfusion/ej2-grids").then(Grid => {
+    debugger;
+    console.log(Grid);
   });
-  grid.appendTo('#grid');
-});
 }
